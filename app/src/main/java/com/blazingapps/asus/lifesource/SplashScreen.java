2ed4dcf095ee499240e7f12d773de5233d15e1c6
Navaneeth <navaneethsaj@gmail.com>
2018-11-25 16:32:38 +0530

    sms permission

--- app/src/main/java/com/blazingapps/asus/lifesource/SplashScreen.java
+++ app/src/main/java/com/blazingapps/asus/lifesource/SplashScreen.java
@@ -1,15 +1,22 @@
 package com.blazingapps.asus.lifesource;
 
+import android.Manifest;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
 import android.os.Handler;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class SplashScreen extends AppCompatActivity {
 
     private static final int SPLASH_DISPLAY_LENGTH = 1500;
+    private static final int MY_PERMISSIONS_REQUEST_SEND_SMS = 0;
     SharedPreferences sharedPreferences;
     private static final String MYPREF = "mypreferences";
     private static final String NAME = "name";
@@ -33,6 +40,17 @@ public class SplashScreen extends AppCompatActivity {
 //        Intent mainIntent = new Intent(SplashScreen.this,MapActivityNav.class);
 //        startActivity(mainIntent);
 //        finish();
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.SEND_SMS)
+                != PackageManager.PERMISSION_GRANTED) {
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                    Manifest.permission.SEND_SMS)) {
+            } else {
+                ActivityCompat.requestPermissions(this,
+                        new String[]{Manifest.permission.SEND_SMS},
+                        MY_PERMISSIONS_REQUEST_SEND_SMS);
+            }
+        }
         new Handler().postDelayed(new Runnable(){
             @Override
             public void run() {
@@ -61,5 +79,17 @@ public class SplashScreen extends AppCompatActivity {
 //                finish();
             }
         }, SPLASH_DISPLAY_LENGTH);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode==MY_PERMISSIONS_REQUEST_SEND_SMS && grantResults.length>0){
+
+
+        }else {
+
+            Toast.makeText(getApplicationContext(),"Permission Needed",Toast.LENGTH_SHORT).show();
+        }
     }
 }
